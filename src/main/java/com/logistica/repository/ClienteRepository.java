9eaea10a64070a042267d9e4bf216b36112dab2a
JonasMoura-J <jonasmourat9090@gmail.com>
2021-08-15 13:48:16 -0300

    implementando interface repository de Cliente com metodos de consulta

--- src/main/java/com/logistica/repository/ClienteRepository.java
+++ src/main/java/com/logistica/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.logistica.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.logistica.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-
+	
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
 }
